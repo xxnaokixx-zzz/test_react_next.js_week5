c7228dfb930e18d753db809d51983075bace082c
加藤　直起 <Naoki@MOE-OGAWA.local>
2025-04-19 21:06:07 +0900

    Add dynamic routing for posts

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/test_react_next.js_week5' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/test_react_next.js_week5/' : '',
-  trailingSlash: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
