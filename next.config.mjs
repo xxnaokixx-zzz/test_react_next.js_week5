7f3bc730c214423758f6f42f3d753f9765969c3e
加藤　直起 <Naoki@MOE-OGAWA.local>
2025-04-19 19:22:42 +0900

    Add GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/test_react_next.js_week5',
+  assetPrefix: '/test_react_next.js_week5/',
+};
 
 export default nextConfig;
